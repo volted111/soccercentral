b060aae15e417ae6238a2a32b70317867e78e7dd
Daniel K <danielkeelagher@gmail.com>
2020-07-19 12:20:57 +1000

    Added team component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import React from 'react';
 // component imports
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { ALeagueTeam } from "./components/ALeagueTeam"
 
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <ALeagueTeam/>
             <Footer/>
+            
         </div>
     );
 }
