f588787060eeecb70c47a562067001594fb7e320
Luke Fisher <lukefisher@Lukes-MBP.lan>
2020-07-25 13:07:52 +1000

    fix formatting

--- src/components/ALeagueTeam/index.js
+++ src/components/ALeagueTeam/index.js
@@ -3,62 +3,62 @@ import React from 'react';
 export const ALeagueTeam = () => {
     return (
         <div>
-<section id="one" class="tiles">
-								<article>
+            <section id="one" class="tiles">
+                <article>
 									<span class="image">
-										<img src="images/pic01.jpg" alt="" />
+										<img src="images/pic01.jpg" alt=""/>
 									</span>
-									<header class="major">
-										<h3><a href="landing.html" class="link">Aliquam</a></h3>
-										<p>Ipsum dolor sit amet</p>
-									</header>
-								</article>
-								<article>
+                    <header class="major">
+                        <h3><a href="landing.html" class="link">Aliquam</a></h3>
+                        <p>Ipsum dolor sit amet</p>
+                    </header>
+                </article>
+                <article>
 									<span class="image">
-										<img src="images/pic02.jpg" alt="" />
+										<img src="images/pic02.jpg" alt=""/>
 									</span>
-									<header class="major">
-										<h3><a href="landing.html" class="link">Tempus</a></h3>
-										<p>feugiat amet tempus</p>
-									</header>
-								</article>
-								<article>
+                    <header class="major">
+                        <h3><a href="landing.html" class="link">Tempus</a></h3>
+                        <p>feugiat amet tempus</p>
+                    </header>
+                </article>
+                <article>
 									<span class="image">
-										<img src="images/pic03.jpg" alt="" />
+										<img src="images/pic03.jpg" alt=""/>
 									</span>
-									<header class="major">
-										<h3><a href="landing.html" class="link">Magna</a></h3>
-										<p>Lorem etiam nullam</p>
-									</header>
-								</article>
-								<article>
+                    <header class="major">
+                        <h3><a href="landing.html" class="link">Magna</a></h3>
+                        <p>Lorem etiam nullam</p>
+                    </header>
+                </article>
+                <article>
 									<span class="image">
-										<img src="images/pic04.jpg" alt="" />
+										<img src="images/pic04.jpg" alt=""/>
 									</span>
-									<header class="major">
-										<h3><a href="landing.html" class="link">Ipsum</a></h3>
-										<p>Nisl sed aliquam</p>
-									</header>
-								</article>
-								<article>
+                    <header class="major">
+                        <h3><a href="landing.html" class="link">Ipsum</a></h3>
+                        <p>Nisl sed aliquam</p>
+                    </header>
+                </article>
+                <article>
 									<span class="image">
-										<img src="images/pic05.jpg" alt="" />
+										<img src="images/pic05.jpg" alt=""/>
 									</span>
-									<header class="major">
-										<h3><a href="landing.html" class="link">Consequat</a></h3>
-										<p>Ipsum dolor sit amet</p>
-									</header>
-								</article>
-								<article>
+                    <header class="major">
+                        <h3><a href="landing.html" class="link">Consequat</a></h3>
+                        <p>Ipsum dolor sit amet</p>
+                    </header>
+                </article>
+                <article>
 									<span class="image">
-										<img src="images/pic06.jpg" alt="" />
+										<img src="images/pic06.jpg" alt=""/>
 									</span>
-									<header class="major">
-										<h3><a href="landing.html" class="link">Etiam</a></h3>
-										<p>Feugiat amet tempus</p>
-									</header>
-								</article>
-							</section>
+                    <header class="major">
+                        <h3><a href="landing.html" class="link">Etiam</a></h3>
+                        <p>Feugiat amet tempus</p>
+                    </header>
+                </article>
+            </section>
         </div>
     );
 };
