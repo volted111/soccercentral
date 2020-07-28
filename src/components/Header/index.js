de1290406e657f6d7c1b3580fa115e9f095e8f74
Daniel K <danielkeelagher@gmail.com>
2020-07-28 20:42:26 +1000

    message

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
 export const Header = () => {
-    return (
+    <div>
+    <section id="banner" class="major">
+    <div class="inner">
+        <header class="major">
+            <h1>Hi, my name is Forty</h1>
+        </header>
+        <div class="content">
+            <p>A responsive site template designed by HTML5 UP<br />
+            and released under the Creative Commons.</p>
+            <ul class="actions">
+                <li><a href="#one" class="button next scrolly">Get Started</a></li>
+            </ul>
+        </div>
+    </div>
+</section> 
         <div>
             <header id="header" className="alt">
                 <a href="index.html" className="logo"><strong>Forty</strong> <span>by HTML5 UP</span></a>
